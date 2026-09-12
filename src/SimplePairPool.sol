e29f0d8b77d8826736cb5f67a8f9c679dfe92e8f
Abhishek Maurya <abhiofficialwork96@gmail.com>
2026-09-12 01:39:56 +0000

    fix: correct OpenZeppelin Math import path

--- src/SimplePairPool.sol
+++ src/SimplePairPool.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {Math} from "@openzeppelin/contracts/utils/Math/Math.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract SimplePairPool {
